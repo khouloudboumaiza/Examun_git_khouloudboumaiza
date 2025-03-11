648d2a630157d9b9a3ef0157ef166e8932db326f
khouloud boumaiza <khouloudboumaiza67@gmail.com>
2025-03-11 11:18:33 +0100

    gerer conflit

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ hello,git!
 <<<<<<< HEAD
 modification de fichier 
 modification d'apres main
-=======
+
 modification d'apres feature_x
->>>>>>> f29921c (ajout de l'affichage de la date et l'heure feature)
+ HEAD
+ f29921c (ajout de l'affichage de la date et l'heure feature)
+
+<<<<<<< HEAD
+>>>>>>> feature_x
+=======
+dernier commit pour fusionner
+>>>>>>> feature_x
+>>>>>>> 329346e (commit for push)
