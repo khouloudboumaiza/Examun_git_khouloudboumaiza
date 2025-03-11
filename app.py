f43086e2204f657d1a7362e5778c4b42c112a9bb
khouloud boumaiza <khouloudboumaiza67@gmail.com>
2025-03-11 10:29:58 +0100

    ajout de l'affichage de la date et l'heure feature hhhhh

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 hello,git!
+<<<<<<< HEAD
 modification de fichier 
 modification d'apres main
+=======
+modification d'apres feature_x
+>>>>>>> f29921c (ajout de l'affichage de la date et l'heure feature)
